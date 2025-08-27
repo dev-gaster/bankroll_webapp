ce987347b0452e9dd079b5024d1cd87bd5fd3372
dev-gaster <devgaster@gmail.com>
2025-08-27 12:28:48 +0300

    search-form-added

---
+++ components/SearchForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-dialog :fullscreen="$vuetify.display.mobile" max-width="600">
+    <template #activator="{ props: activatorProps }">
+      <v-card
+        v-if="$vuetify.display.smAndUp"
+        v-bind="activatorProps"
+        class="pa-2 d-flex"
+        width="250"
+        height="40"
+        color="grey lighten-4"
+        variant="tonal"
+        autofocus
+        rounded
+        style="cursor: pointer; align-items: center; gap: 8px; border-radius: 20px"
+      >
+        <v-icon>mdi-magnify</v-icon>
+        <div>Search Bankroll</div>
+      </v-card>
+      <v-btn v-else icon v-bind="activatorProps" variant="text">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </template>
+    <template #default="{ isActive }">
+      <v-card
+        :rounded="$vuetify.display.mobile ? false : true"
+        :style="{ borderRadius: $vuetify.display.mobile ? '0px' : '10px' }"
+      >
+        <v-toolbar flat color="white">
+          <v-text-field
+            v-model="searchQuery"
+            append-inner-icon="mdi-close"
+            variant="solo"
+            tile
+            single-line
+            hide-details
+            flat
+            color="primary"
+            :placeholder="`Search ${tab} on bankroll `"
+            :aria-placeholder="`Search ${tab} on bankroll `"
+            prepend-inner-icon="mdi-magnify"
+            @click:append-inner="isActive.value = !isActive"
+          />
+          <template #extension>
+            <v-tabs v-model="tab" align-tabs="title" density="compact">
+              <v-tab v-for="item in items" :key="item" :text="item" :value="item" />
+            </v-tabs>
+          </template>
+        </v-toolbar>
+        <v-divider />
+        <v-card-text
+          class="mt-4"
+          :style="{
+            height: $vuetify.display.mobile ? '100vh' : '85vh',
+          }"
+        >
+          <!-- show search results here -->
+        </v-card-text>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+<script setup lang="ts">
+const tab = ref(null);
+const searchQuery = ref("");
+const items = ["campaigns", "people", "communities"];
+</script>
+<style lang="scss" scoped>
+.no-border-field .v-input__slot {
+  border: none !important;
+  box-shadow: none !important;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,67 @@
 <template>
   <v-app>
     <v-app-bar color="transparent" flat>
-      <template #prepend>
-        <v-toolbar color="transparent" flat>
-          <NuxtLink to="/">
-            <v-img src="/bankroll-logo.png" alt="Company Logo" :width="$vuetify.display.mobile ? '100' : '150'" cover />
-          </NuxtLink>
-        </v-toolbar>
-      </template>
-      <v-spacer />
       <div class="d-none d-sm-flex">
-        <v-btn variant="text" class="font-weight-medium" to="/campaigns"> Campaign</v-btn>
-        <v-btn variant="text" class="font-weight-medium" to="/features"> Features</v-btn>
-        <v-btn variant="text" class="font-weight-medium" to="/contact"> Contact Us</v-btn>
+        <v-btn variant="text" rounded class="font-weight-medium" to="/faq"> FAQ</v-btn>
+        <v-btn variant="text" rounded class="font-weight-medium" to="/campaigns">
+          Campaign</v-btn
+        >
+        <!-- <v-btn variant="text" class="font-weight-medium" to="/features"> Features</v-btn> -->
+        <v-btn variant="text" rounded class="font-weight-medium"> Resources</v-btn>
       </div>
       <v-spacer />
-      <v-btn height="40" rounded class="font-weight-medium d-none d-md-flex" style="font-size: 16px" color="primary"
-        variant="flat" depressed target="blank" href="https://forms.gle/6nPDEWTNcaVwnQLY6">
+      <NuxtLink to="/">
+        <v-img
+          src="/bankroll-logo.png"
+          alt="Company Logo"
+          :width="$vuetify.display.mobile ? '100' : '150'"
+          cover
+        />
+      </NuxtLink>
+
+      <v-spacer />
+
+      <div class="mr-3">
+        <search-form />
+      </div>
+      <v-btn
+        height="40"
+        rounded
+        class="font-weight-medium d-none d-md-flex"
+        style="font-size: 16px"
+        color="primary"
+        variant="flat"
+        depressed
+        target="blank"
+        href="https://forms.gle/6nPDEWTNcaVwnQLY6"
+      >
         Get the App
       </v-btn>
-      <v-btn v-if="$vuetify.display.mobile" class="d-flex d-sm-none" target="blank"
-        href="https://forms.gle/6nPDEWTNcaVwnQLY6" icon outlined height="35" width="35"
-        color="primary"><v-icon>mdi-download</v-icon></v-btn>
+      <v-btn
+        v-if="$vuetify.display.mobile"
+        class="d-flex d-sm-none"
+        target="blank"
+        href="https://forms.gle/6nPDEWTNcaVwnQLY6"
+        icon
+        outlined
+        height="35"
+        width="35"
+        color="primary"
+        ><v-icon>mdi-download</v-icon></v-btn
+      >
     </v-app-bar>
     <!-- <NavBar v-if="$vuetify.display.smAndDown" /> -->
     <v-main>
       <slot />
     </v-main>
-    <v-footer v-if="
-      !route.path.includes('campaigns') &&
-      !route.path.includes('login') &&
-      !$route.path.includes('signup')
-    " color="#00031C">
+    <v-footer
+      v-if="
+        !route.path.includes('campaigns') &&
+        !route.path.includes('login') &&
+        !$route.path.includes('signup')
+      "
+      color="#00031C"
+    >
       <SiteFooter />
     </v-footer>
   </v-app>
