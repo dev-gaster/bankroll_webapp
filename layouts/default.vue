b621d8b3ae61717b508e7a96566011ab51aaf508
dev-gaster <devgaster@gmail.com>
2025-12-06 07:11:07 +0300

    UI improvemeny

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,7 +28,7 @@
         <v-divider />
         <div>
           <v-btn target="blank" color="primary" rounded variant="flat" class="ma-4"
-            href="https://forms.gle/6nPDEWTNcaVwnQLY6">
+            href="https://play.google.com/store/apps/details?id=com.gasobu.fintech_apps.bankroll&pcampaignid=web_share">
             Get the App
           </v-btn>
         </div>
@@ -38,7 +38,13 @@
       <v-btn icon variant="text" class="d-flex d-sm-none" @click="drawer = !drawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
-      <div class="d-none d-sm-flex">
+      <NuxtLink to="/">
+        <v-img src="/bankroll-logo.png" alt="Company Logo" :width="$vuetify.display.mobile ? '100' : '150'" cover />
+      </NuxtLink>
+
+      <v-spacer />
+      <div class="d-none d-sm-flex" style="border: 0.5px solid grey; border-radius: 50px; padding: 8px">
+        <v-btn variant="text" rounded class="font-weight-medium" to="/about">About</v-btn>
         <v-btn variant="text" rounded class="font-weight-medium" to="/faq"> FAQ</v-btn>
         <v-btn variant="text" rounded class="font-weight-medium" to="/campaigns">
           Campaign</v-btn>
@@ -46,22 +52,18 @@
         <v-btn variant="text" rounded class="font-weight-medium"> Resources</v-btn>
       </div>
       <v-spacer />
-      <NuxtLink to="/">
-        <v-img src="/bankroll-logo.png" alt="Company Logo" :width="$vuetify.display.mobile ? '100' : '150'" cover />
-      </NuxtLink>
-
-      <v-spacer />
 
       <!-- <div class="mr-3">
         <search-form />
       </div> -->
       <v-btn height="40" rounded class="font-weight-medium d-none d-md-flex" style="font-size: 16px" color="primary"
-        variant="flat" depressed target="blank" href="https://forms.gle/6nPDEWTNcaVwnQLY6">
+        variant="flat" depressed target="_blank"
+        href="https://play.google.com/store/apps/details?id=com.gasobu.fintech_apps.bankroll&pcampaignid=web_share">
         Get the App
       </v-btn>
-      <v-btn v-if="$vuetify.display.mobile" class="d-flex d-sm-none" target="blank"
-        href="https://play.google.com/apps/testing/com.gasobu.fintech_apps.bankroll" icon outlined height="35"
-        width="35" color="primary"><v-icon>mdi-download</v-icon></v-btn>
+      <v-btn v-if="$vuetify.display.mobile" class="d-flex d-sm-none" target="_blank"
+        href="https://play.google.com/store/apps/details?id=com.gasobu.fintech_apps.bankroll&pcampaignid=web_share" icon
+        outlined height="35" width="35" color="primary"><v-icon>mdi-download</v-icon></v-btn>
     </v-app-bar>
     <!-- <NavBar v-if="$vuetify.display.smAndDown" /> -->
     <v-main>
