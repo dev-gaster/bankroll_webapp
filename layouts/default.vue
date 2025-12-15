9471231bc8903412d85a938307b0b4f399f64459
dev-gaster <devgaster@gmail.com>
2025-12-15 12:31:18 +0300

    revampted navigation-drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,11 @@
     <!-- navigation drawer only on small devices -->
     <v-navigation-drawer v-model="drawer" class="d-flex d-sm-none" width="400" temporary>
       <template #prepend>
-        <v-toolbar flat color="transparent">
+        <v-toolbar flat color="transparent" class="d-flex align-center justify-space-between">
+          <!-- logo -->
+          <NuxtLink to="/">
+            <v-img src="/bankroll-logo.png" alt="Bankroll Logo" width="120" cover />
+          </NuxtLink>
           <v-btn icon variant="text" @click="drawer = false">
             <v-icon>mdi-close</v-icon>
           </v-btn>
@@ -11,51 +15,27 @@
         <v-divider />
       </template>
       <v-list nav>
-        <v-list-item to="/faq">
-          <v-list-item-title>FAQ</v-list-item-title>
-        </v-list-item>
-        <v-list-item to="/campaigns">
-          <v-list-item-title>Campaigns</v-list-item-title>
-        </v-list-item>
-        <!-- <v-list-item to="/features">
-        <v-list-item-title>Features</v-list-item-title>
-      </v-list-item> -->
-        <v-list-item to="/resources">
-          <v-list-item-title>Resources</v-list-item-title>
+        <v-list-item v-for="(link, index) in links" :key="index" :to="link.to" link>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
       <template #append>
         <v-divider />
         <div>
-          <v-btn
-            target="blank"
-            color="primary"
-            rounded
-            variant="flat"
-            class="ma-4"
-            href="https://play.google.com/store/apps/details?id=com.gasobu.fintech_apps.bankroll&pcampaignid=web_share"
-          >
+          <v-btn target="blank" color="primary" rounded variant="flat" class="ma-4"
+            href="https://play.google.com/store/apps/details?id=com.gasobu.fintech_apps.bankroll&pcampaignid=web_share">
             Get the App
           </v-btn>
         </div>
       </template>
     </v-navigation-drawer>
-    <v-app-bar
-      color="transparent"
-      flat
-      :scroll-behavior="route.path.includes('campaigns') ? 'hide' : 'none'"
-      :class="{ 'on-dark': isOnDark }"
-    >
+    <v-app-bar color="transparent" flat :scroll-behavior="route.path.includes('campaigns') ? 'hide' : 'none'"
+      :class="{ 'on-dark': isOnDark }">
       <v-btn icon variant="text" class="d-flex d-sm-none" @click="drawer = !drawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
       <NuxtLink to="/">
-        <v-img
-          src="/bankroll-logo.png"
-          alt="Company Logo"
-          :width="$vuetify.display.mobile ? '100' : '150'"
-          cover
-        />
+        <v-img src="/bankroll-logo.png" alt="Company Logo" :width="$vuetify.display.mobile ? '100' : '150'" cover />
       </NuxtLink>
 
       <v-spacer />
@@ -69,17 +49,9 @@
       <!-- <div class="mr-3">
         <search-form />
       </div> -->
-      <v-btn
-        height="40"
-        class="font-weight-medium text-white"
-        prepend-icon="mdi-arrow-down"
-        style="font-size: 16px"
-        color="primary"
-        variant="flat"
-        depressed
-        target="_blank"
-        href="https://play.google.com/store/apps/details?id=com.gasobu.fintech_apps.bankroll&pcampaignid=web_share"
-      >
+      <v-btn height="40" class="font-weight-medium text-white" prepend-icon="mdi-arrow-down" style="font-size: 16px"
+        color="primary" variant="flat" depressed target="_blank"
+        href="https://play.google.com/store/apps/details?id=com.gasobu.fintech_apps.bankroll&pcampaignid=web_share">
         Get the App
       </v-btn>
     </v-app-bar>
@@ -87,14 +59,11 @@
     <v-main>
       <slot />
     </v-main>
-    <v-footer
-      v-if="
-        !route.path.includes('campaigns') &&
-        !route.path.includes('login') &&
-        !$route.path.includes('signup')
-      "
-      color="#00031C"
-    >
+    <v-footer v-if="
+      !route.path.includes('campaigns') &&
+      !route.path.includes('login') &&
+      !$route.path.includes('signup')
+    " color="#00031C">
       <SiteFooter />
     </v-footer>
   </v-app>
